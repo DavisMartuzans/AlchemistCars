188f96605b8d9115fd75ee7e4e2c6a4b9bb8d310
Davis Martuzans <64132861+DavisMartuzans@users.noreply.github.com>
2023-05-22 21:38:07 +0300

    meh

--- signin.php
+++ signin.php
@@ -1,12 +1,45 @@
+<?php
+session_start();
+
+require_once('db_credentials.php');
+
+// Establish database connection
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$db) {
+  // Handle connection error
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $myemail = mysqli_real_escape_string($db, $_POST['email']);
+  $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+  $res = mysqli_query($db, "SELECT id, password, admin FROM users WHERE email = '$myemail'");
+  $row = mysqli_fetch_assoc($res);
+
+  if (!filter_var($myemail, FILTER_VALIDATE_EMAIL)) {
+    $error = "Please enter a valid email address.";
+  } elseif (empty($myemail) || empty($mypassword)) {
+    $error = "Please enter your email and password.";
+  } elseif (!$row || !password_verify($mypassword, $row['password'])) {
+    $error = "Your Login Email or Password is invalid";
+  } else {
+    $_SESSION['login_user'] = $myemail;
+    header($row['admin'] ? "Location: admin_dashboard.php" : "Location: main.php");
+    exit(); // Ensure script execution is stopped after redirecting
+  }
+}
+
+// Close the database connection
+mysqli_close($db);
+?>
 <!DOCTYPE html>
 <html>
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Sign in</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+	<title>Sign In</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <header>
   <img src="Components/AlchemistCars.PNG" alt="Company Logo" id="logo">
@@ -22,55 +55,19 @@
   </nav>
 </header>
 <body>
-<form method="POST" action="signin.php">
-	<label for="username">Username:</label>
-	<input type="text" id="username" name="username"><br>
 
-	<label for="password">Password:</label>
-	<input type="password" id="password" name="password"><br>
+  <div class="center-form">
+	<form action="" method="post">
+		<label for="email">Email:</label><br>
+		<input type="email" name="email" class = "box"><br>
 
-	<label for="user_type">User type:</label>
-	<select id="user_type" name="user_type">
-		<option value="user">User</option>
-		<option value="admin">Admin</option>
-	</select><br>
+		<label for="password">Password:</label><br>
+		<input type="password" name="password" class = "box"><br><br>
 
-	<input type="submit" value="Sign in">
-  <a href="signup.php">Don't have a account!</a>
-</form>
-  <?php
-
-require_once('db_credentials.php');
-
-// Get form data
-$username = $_POST["username"];
-$password = $_POST["password"];
-$user_type = $_POST["user_type"];
-
-// Prepare SQL statement
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password' AND user_type='$user_type'";
-
-// Execute SQL statement
-$result = $conn->query($sql);
-
-// Check if user exists
-if ($result->num_rows > 0) {
-	// User exists, redirect to dashboard
-	if ($user_type == "admin") {
-		header("Location: admin_dashboard.php");
-	} else {
-		header("Location: user_dashboard.php");
-	}
-} else {
-	// User doesn't exist, show error message
-	echo "Invalid username or password.";
-}
-
-
-// Close database connection
-$conn->close();
-?>
-
-
+		<input type="submit" value="Sign in">
+    <a href=signup.php>I dont have a account!</a>
+	</form>
+  <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if (isset($error)){echo $error;}; ?></div>
+  </div>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,11 +1,53 @@
+<?php
+session_start();
+require_once('db_credentials.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $myname = $_POST['name'];
+  $myemail = $_POST['email'];
+  $mypassword = $_POST['password'];
+
+  // Validate user input
+  if (!filter_var($myemail, FILTER_VALIDATE_EMAIL)) {
+    $error = "Please enter a valid email address.";
+  } elseif (empty($myname) || empty($myemail) || empty($mypassword)) {
+    $error = "Please enter your name, email, and password.";
+  } else {
+    // Check if email already exists
+    $chk_query = "SELECT * FROM users WHERE email = ?";
+    $stmt = mysqli_prepare($db, $chk_query);
+    mysqli_stmt_bind_param($stmt, "s", $myemail);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($result) > 0) {
+      $error = "Error: Email already exists in the database.";
+    } else {
+      // Insert user data into the database
+      $insert_query = "INSERT INTO users (name, email, password, admin) VALUES (?, ?, ?, 0)";
+      $stmt = mysqli_prepare($db, $insert_query);
+      $hashed_password = password_hash($mypassword, PASSWORD_DEFAULT);
+      mysqli_stmt_bind_param($stmt, "sss", $myname, $myemail, $hashed_password);
+      if (mysqli_stmt_execute($stmt)) {
+        $_SESSION['login_user'] = $myemail;
+        header("Location: main.php");
+        exit();
+      } else {
+        echo "Error: " . mysqli_error($db);
+      }
+    }
+  }
+  mysqli_close($db);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Sign Up</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
+	<title>Register</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <header>
   <img src="Components/AlchemistCars.PNG" alt="Company Logo" id="logo">
@@ -21,45 +63,20 @@
   </nav>
 </header>
 <body>
-  <h1>Sign Up</h1>
-  <form method="POST" action="signup.php">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-    <br><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <br><br>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
-    <br><br>
-    <input type="submit" value="Sign Up">
-  </form>
+  <div class="center-form">
+	<form action="" method="post">
+    <label for="name">Name:</label><br>
+		<input type="name" name="name" class = "box"><br>
 
-  <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  $email = $_POST["email"];
+		<label for="email">Email:</label><br>
+		<input type="email" name="email" class = "box"><br>
 
-  require_once('db_credentials.php');
+		<label for="password">Password:</label><br>
+		<input type="password" name="password" class = "box"><br><br>
 
-
-  // prepare statement and bind parameters
-  $stmt = mysqli_prepare($conn, "INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-  mysqli_stmt_bind_param($stmt, "sss", $username, $password, $email);
-
-  // execute statement
-  if (mysqli_stmt_execute($stmt)) {
-    echo "<p>Sign up successful!</p>";
-  } else {
-    echo "<p>Error: " . mysqli_stmt_error($stmt) . "</p>";
-  }
-
-  // close statement and connection
-  mysqli_stmt_close($stmt);
-  mysqli_close($conn);
-}
-?>
-
+		<input type="submit" value="Register">
+    <div style = "font-size:14px; color:#cc0000; margin-top:10px"><?php if (isset($error)){echo $error;}; ?></div>
+	</form>
+  </div>
 </body>
 </html>
