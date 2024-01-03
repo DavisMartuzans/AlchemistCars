a8e8bfbca21972b0ab8fb2eaac4ab93d1b25007f
Davis Martuzans <64132861+DavisMartuzans@users.noreply.github.com>
2024-01-03 19:36:10 +0200

    Comments for smooth scrolling function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Gludas ritināšanas funkcija
 document.addEventListener("DOMContentLoaded", makeLinksSmooth);
 function makeLinksSmooth() { 
     const navLinks = document.querySelectorAll("section a"); 
@@ -9,11 +10,14 @@ function makeLinksSmooth() {
 
   function smoothScroll(e) {
     e.preventDefault();
+    // Dabū mērķa elementa ID no Href atribūta
     const targetId = this.getAttribute("href");
+
+    // Atrod mērķa elementu DOM izmantojot ID
     const targetElement = document.querySelector(targetId);
-  
+
+    // Ja mērķa elements eksistē tad izmanto gludo ritināšanas funckiju
     if (targetElement) { 
-      targetElement.scrollIntoView({ behavior: "smooth", });
+        targetElement.scrollIntoView({ behavior: "smooth", block: "end" }); 
     }
   }
-targetElement.scrollIntoView({ behavior: "smooth", block: "end" }); 
