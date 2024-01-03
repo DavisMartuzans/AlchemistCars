46ddc4089b9228700c5d9b65a758bcb617e00fd7
Davis Martuzans <64132861+DavisMartuzans@users.noreply.github.com>
2024-01-03 19:40:30 +0200

    Comments added

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $conn->close();
       
       
    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Account
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -92,13 +92,12 @@ $conn->close();
         <h2 id="welcome">Welcome to AlchemistCars</h2>
         <?php
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-            // For signed-in users
+            // Priekš lietotājiem
             echo '<p class="about">You are logged in as ' . $_SESSION['username'] . '</p>';
             echo '<p class="about">Here you can find a variety of awesome cars.</p>';
             echo '<p class="about">If you find anything you like, contact us and meet us.</p>';
-            // Additional content for logged-in users
         } else {
-            // For non-logged-in users
+            // Priekš viesiem
             echo '<p class="about">Here you can find a variety of awesome cars.</p>';
             echo '<p class="about">If you find anything you like, please sign in to access additional features.</p>';
         }
@@ -210,107 +209,107 @@ $conn->close();
     padding: 20px;
     text-align: center;
 }
-    /* Laipni lūdzam teksts */
-    #welcome{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 60px;
-        font-weight:bold;
-        color: white;
-        text-align: center;
-        margin-bottom: 20px;
-    }
+/* Laipni lūdzam teksts */
+#welcome{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 60px;
+    font-weight:bold;
+    color: white;
+    text-align: center;
+    margin-bottom: 20px;
+}
 
-    .about {
-        font-size: 20px;
-        color: white;
-        text-align: center;
-        margin-bottom: 20px;
-    }
+.about {
+    font-size: 20px;
+    color: white;
+    text-align: center;
+    margin-bottom: 20px;
+}
 
-    /* Mašīnu filtrs */
-    #car-filter {
-        margin: 0 auto;
-        max-width: 600px;
-        padding: 20px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
+/* Mašīnu filtrs */
+#car-filter {
+    margin: 0 auto;
+    max-width: 600px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
 
-    #car-filter form {
-        display: inline-flex;
-        flex-direction: row;
-        gap: 10px;
-    }
+#car-filter form {
+    display: inline-flex;
+    flex-direction: row;
+    gap: 10px;
+}
 
-    #car-filter label {
-        flex-basis: 100%;
-    }
+#car-filter label {
+    flex-basis: 100%;
+}
 
-    #car-filter select,
-    #car-filter input[type="submit"] {
-        margin: 10px 0;
-        padding: 5px;
-        border-radius: 5px;
-    }
+#car-filter select,
+#car-filter input[type="submit"] {
+    margin: 10px 0;
+    padding: 5px;
+    border-radius: 5px;
+}
 
-    #car-filter select {
-        flex-basis: calc(33.33% - 10px);
-    }
+#car-filter select {
+    flex-basis: calc(33.33% - 10px);
+}
 
-    #car-filter input[type="submit"] {
-        flex-basis: 100%;
-        background-color: #454545;
-        color: #fff;
-        border: none;
-        cursor: pointer;
-    }
+#car-filter input[type="submit"] {
+    flex-basis: 100%;
+    background-color: #454545;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
 
-    #car-filter input[type="submit"]:hover {
-        background-color: #454545;
-    }
+#car-filter input[type="submit"]:hover {
+    background-color: #454545;
+}
 
-    #filterButton {
-        width: 100%;
-        padding: 10px;
-        background-color: #454545;
-        color: white;
-        font-size: 16px;
-        border: none;
-        cursor: pointer;
-    }
+#filterButton {
+    width: 100%;
+    padding: 10px;
+    background-color: #454545;
+    color: white;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+}
 
-    #filterButton:hover {
-        background-color: #000;
-    }
+#filterButton:hover {
+    background-color: #000;
+}
 
-    /* Mašīnu kārtis */
-    .card {
-        width: 300px;
-        height: 400px;
-        box-shadow: 2px 2px 5px #ccc;
-        border-radius: 10px;
-        overflow: hidden;
-        margin: 10px;
-        display: inline-block;
-    }
+/* Mašīnu kārtis */
+.card {
+    width: 300px;
+    height: 400px;
+    box-shadow: 2px 2px 5px #ccc;
+    border-radius: 10px;
+    overflow: hidden;
+    margin: 10px;
+    display: inline-block;
+}
 
-    .card img {
-        width: 100%;
-        height: 200px;
-        object-fit: cover;
-    }
+.card img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+}
 
-    .card h3 {
-        margin: 10px;
-        font-size: 18px;
-    }
+.card h3 {
+    margin: 10px;
+    font-size: 18px;
+}
 
-    .card p {
-        margin: 10px;
-        font-size: 14px;
-    }
+.card p {
+    margin: 10px;
+    font-size: 14px;
+}
 
 #learn-more {
     display: inline-block;
@@ -329,21 +328,7 @@ $conn->close();
     background-color: #2980b9;
     border-color: #2980b9; 
 }
-/* Pielāgota ritjosla */
-body::-webkit-scrollbar {
-  width: 12px;
-}
-
-body::-webkit-scrollbar-track {
-  background: whitesmoke;
-}
-
-body::-webkit-scrollbar-thumb {
-  background-color:lightgray;
-  border-radius: 20px;
-  border: 2px gray;
-}
 </style>
-<script src="script.js"></script>
+<script src="./script.js"></script>
 </body>
 </html>
